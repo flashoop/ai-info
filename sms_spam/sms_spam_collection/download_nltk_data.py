d38ebb5bd9a1b6273d825c243ec5010b3d38b9d1
Your Name <you@example.com>
2026-01-26 13:19:32 +0000

    dataprocess commit

---
+++ sms_spam/sms_spam_collection/download_nltk_data.py
@@ -0,0 +1,8 @@
+import nltk
+
+# Download required NLTK data
+print("Downloading NLTK data...")
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+print("NLTK data downloaded successfully!")
